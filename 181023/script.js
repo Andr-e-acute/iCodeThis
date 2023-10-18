718c95dee22e072f9dab625f5dc710907bf743ba
André Potthast <andre.potthast.ap@googlemail.com>
2023-10-18 20:33:43 +0200

    not finished desgin not functional

---
+++ 181023/script.js
@@ -0,0 +1,6 @@
+// // Make the DIV element draggable:
+// const cards = document.querySelectorAll(".card");
+// cards.forEach((card) => {
+//   const movableZone = card.querySelector(".movableZone");
+//   movableZone.addEventListener("mousedown");
+// });
