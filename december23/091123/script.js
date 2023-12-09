4d0534319575205167d1ca3ca8c4b4e84946d4c2
André Potthast <andre.potthast.ap@googlemail.com>
2023-12-09 20:29:07 +0100

    i code this 09.12.23 style done

---
+++ december23/091123/script.js
@@ -0,0 +1,4 @@
+const selectBtn = document.querySelector(".select-button");
+selectBtn.addEventListener("click", () => {
+  document.querySelector(".custom-dropdown").classList.toggle("active");
+});
