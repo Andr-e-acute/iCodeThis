485bcc3192626de2f47b704ef4c256f3b7d99ee3
André Potthast <andre.potthast.ap@googlemail.com>
2024-01-13 08:15:23 +0100

    iCodeThis 13.01.24 html done

---
+++ january24/120124/script.js
@@ -0,0 +1,12 @@
+const draggables = document.querySelectorAll(".card[draggable = 'true']");
+
+draggables.forEach((card) => {
+  card.addEventListener("dragend", (event) => {
+    const height = event.target.offsetHeight / 2;
+    const width = event.target.offsetWidth / 2;
+    console.log(width);
+    console.log(event.x);
+    card.style.top = `${event.y - height}px`;
+    card.style.left = `${event.x - width}px`;
+  });
+});
