d1f81abeec9181520b1b5e9b2e147dcebf28663c
André Potthast <andre.potthast.ap@googlemail.com>
2024-02-17 12:18:04 +0100

    i Code this 17.02.24 done looks ok and functional

--- febuary24/17/script.js
+++ febuary24/17/script.js
@@ -8,27 +8,43 @@ const profilePicUrl = [
   "https://source.unsplash.com/a19OVaa2rzA",
   "https://source.unsplash.com/KBv5dEN3QtY",
 ];
+const prevButt = document.querySelector("#prev");
+const nextButt = document.querySelector("#next");
 
-let currentImg = 6;
+let currentImg = 2;
 function createImg(index) {
   const img = document.createElement("img");
   img.src = profilePicUrl[index];
+  img.addEventListener("click", () => {
+    currentImg = index;
+    createImgSet();
+  });
   return img;
 }
 function createImgSet() {
+  profileImages.innerHTML = "";
   // condition when current Img is first img/hide return Butt
   const activeImg = createImg(currentImg);
   activeImg.classList.add("active");
 
   if (currentImg < 1) {
+    prevButt.disabled = true;
+    nextButt.disabled = false;
+    // still can be clicked.
     profileImages.appendChild(activeImg);
     profileImages.appendChild(createImg(currentImg + 1));
     profileImages.appendChild(createImg(currentImg + 2));
+    // hide butt prev
   } else if (currentImg >= profilePicUrl.length - 1) {
+    prevButt.disabled = false;
+    nextButt.disabled = true;
     profileImages.appendChild(createImg(currentImg - 2));
     profileImages.appendChild(createImg(currentImg - 1));
     profileImages.appendChild(activeImg);
+    // hide butt next
   } else {
+    prevButt.disabled = false;
+    nextButt.disabled = false;
     profileImages.appendChild(createImg(currentImg - 1));
     profileImages.appendChild(activeImg);
     profileImages.appendChild(createImg(currentImg + 1));
@@ -37,4 +53,14 @@ function createImgSet() {
   //   condition when current is last /hide next Butt
 }
 
+prevButt.addEventListener("click", () => {
+  currentImg--;
+  createImgSet();
+});
+nextButt.addEventListener("click", () => {
+  currentImg++;
+  createImgSet();
+});
 createImgSet();
+
+let images;
